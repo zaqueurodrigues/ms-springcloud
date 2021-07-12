0f1e830b84c7bc022727a34ef8cc41081f6c43c4
zaqueurodrigues <zakeurodrigues@outlook.com>
2021-07-12 09:42:30 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/com/zaqueurodrigues/hrworker/WorkerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaqueurodrigues.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
