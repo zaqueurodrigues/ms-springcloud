1205727b3c67b23bb922c81f056115a1633176fb
zaqueurodrigues <zakeurodrigues@outlook.com>
2021-07-12 16:38:12 -0300

    Feign client

--- hr-payroll/src/main/java/com/zaqueurodrigues/payroll/PayrollApplication.java
+++ hr-payroll/src/main/java/com/zaqueurodrigues/payroll/PayrollApplication.java
@@ -2,7 +2,10 @@ package com.zaqueurodrigues.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+@EnableFeignClients
 @SpringBootApplication
 public class PayrollApplication {
 
